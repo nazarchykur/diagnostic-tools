e9804aa54b0eae4c1d355d891d4d9576a43c555e
nazarchykur <nazarchykur@gmail.com>
2023-03-07 11:42:55 +0200

    Monitoring Spring Boot Applications With Prometheus and Grafana + Docker

---
+++ sb-prom-grafana/src/main/java/com/example/sbpromgrafana/SbPromGrafanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.sbpromgrafana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbPromGrafanaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbPromGrafanaApplication.class, args);
+    }
+
+}
---
+++ sb-prom-grafana/src/test/java/com/example/sbpromgrafana/SbPromGrafanaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sbpromgrafana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbPromGrafanaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
